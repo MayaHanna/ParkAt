8941b92d56bf86a817cd49d25eaaa1414875f9ff
PazReshef <pazreshef@gmail.com>
2021-03-07 20:08:13 +0200

    add server address to cors whitelist

--- server.js
+++ server.js
@@ -8,11 +8,12 @@ const cors = require("cors");
 let port = 5000;
 
 app.use(cors({
-    origin: "http://localhost:8100",
+    origin: ["localhost:8100", "http://parkat.cs.colman.ac.il:8100"],
     methods: "GET, PATCH, POST, DELETE",
     credentials: true,
     headers: "X-Requested-With, Content-Type, Accept, Authorization, Save-Data, Accept-Ranges"
   }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
